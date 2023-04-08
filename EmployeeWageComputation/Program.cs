a97ae09b8a6ed810ca764c86ff7d46945f43183b
Prachi tote <prachi.tote77@gmail.com>
2023-04-08 10:56:57 +0530

    Adding Welcome Message

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
