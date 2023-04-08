640b125179ec1273e1f93d08c1a7f4f51e7f9a3f
Prachi tote <prachi.tote77@gmail.com>
2023-04-08 12:03:29 +0530

    Calculate Daily Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,20 +8,28 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
+            int empHrs = 0;
             int isPresent = 1;
-            //welcome message
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            //UC-1 Emplyee Attendence
+            int empWage = 0;
+            const int EMP_RATE_PER_HR = 20;
+            //Welcome Message
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            //EmployeeAttendance
             Random random = new Random();
-            int employeeAttendence = random.Next(0, 2); //0 or 1
-            if (employeeAttendence == isPresent)
+            int employeeAttendance = random.Next(0, 2); //0 or 1
+            if (employeeAttendance == isPresent)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
-            else 
+            else
             {
                 Console.WriteLine("Employee is Absent");
+                //empHrs = 0;
             }
+            //CalculateDailyWage
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Daily Wage is: " + empWage);
             Console.ReadLine();
         }
     }
