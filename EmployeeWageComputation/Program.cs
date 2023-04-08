95e096d3b3b32a703bf6ba330e4e3f1a4043b09e
Prachi tote <prachi.tote77@gmail.com>
2023-04-08 11:41:43 +0530

    Checking Employee Attendence

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,8 +8,22 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-
+            int isPresent = 1;
+            //welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            //UC-1 Emplyee Attendence
+            Random random = new Random();
+            int employeeAttendence = random.Next(0, 2); //0 or 1
+            if (employeeAttendence == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else 
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
