74a7fdcd3eed595c242a03b8b9148b60dd3eecfb
Prachi tote <prachi.tote77@gmail.com>
2023-04-10 08:12:16 +0530

    Calculate Wage Till Condition

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,20 +11,22 @@ namespace EmployeeWageComputation
             int empHrs = 0;
             int empWage = 0;
             int totalWage = 0;
-            int day = 0;
+            int day = 1;
+            int totalHrs = 0;
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int NUMBER_OF_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
 
             //Welcome Message
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             //EmployeeAttendance
             Random random = new Random();
-            for (day = 1; day <= NUMBER_OF_WORKING_DAYS; day++)
+            while (day <= NUMBER_OF_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 int employeeAttendance = random.Next(0, 3); //0 to 2
-                //Solving using switch case
+                //Solving using Switch case
                 switch (employeeAttendance)
                 {
                     case IS_FULL_TIME:
@@ -45,11 +47,14 @@ namespace EmployeeWageComputation
                 }
                 //DailyWage
                 empWage = EMP_RATE_PER_HR * empHrs;
-                Console.WriteLine("Employee Wage for day{0}  is:{1} ", day, empWage);
+                Console.WriteLine("Employee Wage for day{0} and {1}hrs is:{2} ", day, empHrs, empWage);
                 //totalWage = totalWage + empWage;
+                day++;
+                totalHrs += empHrs;
                 totalWage += empWage;
             }
-            Console.WriteLine("Total Wage for {0} days in a Month is:{1} ", (day - 1), totalWage);
+            //Console.WriteLine("Employee wage is:" +empWage);
+            Console.WriteLine("Total Wage for {0} days and {1} hrs is:{2} ", (day - 1), totalHrs, totalWage);
             Console.ReadLine();
         }
     }
