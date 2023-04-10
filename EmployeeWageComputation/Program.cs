a2156c7e1d63565632f4620344ef64d2b1f1d45f
Prachi tote <prachi.tote77@gmail.com>
2023-04-10 07:23:22 +0530

    Employee Part Time

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -9,25 +9,32 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             int empHrs = 0;
-            int isPresent = 1;
             int empWage = 0;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
-            //Welcome Message
+
+            //Welcome Message...
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
             //EmployeeAttendance
             Random random = new Random();
-            int employeeAttendance = random.Next(0, 2); //0 or 1
-            if (employeeAttendance == isPresent)
+            int employeeAttendance = random.Next(0, 3); //0 to 2
+            if (employeeAttendance == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Full-time Present");
                 empHrs = 8;
+            }
+            else if (employeeAttendance == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Part-time Present");
+                empHrs = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
                 //empHrs = 0;
             }
-            //CalculateDailyWage
+            //DailyWage
             empWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Employee Daily Wage is: " + empWage);
             Console.ReadLine();
